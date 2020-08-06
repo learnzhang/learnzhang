53b54736d3628363dda0e05103aa45ad13471e34
cort zhang <learn_zhang@163.com>
2020-08-06 17:57:05 +0800

    update sout again

--- src/main/java/com/cort/App.java
+++ src/main/java/com/cort/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "Hello World!" );
+
         System.out.println("this is my first maven project");
         System.out.println("again");
     }
